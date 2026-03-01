4ea342dbbd92a201c3598c154764840464cb2ee3
bc89edij <bc89edij@studserv.uni-leipzig.de>
2026-03-02 00:20:55 +0100

    Adding src folder

---
+++ src/agent_system/agents/base_agent.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 18 23:39:40 2025
+@author: maximilianromberg
+"""
+
+from typing import Dict, Any, List
+import json
+import re
+import os
+from langfuse import get_client
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_agent
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_core.messages import HumanMessage
+from langfuse.langchain import CallbackHandler
+class PlanningAgent:
+
+    def __init__(self, model: str, callback_handler: CallbackHandler):
+        self.model = model
+        self.llm = None
+        self.agent = None
+        self.langfuse_handler = callback_handler
+
+    async def initialize(self):
+        """Async initialization - call this after creating the instance."""
+        print("Initializing Planning Agent")
+        self.llm = ChatOpenAI(
+            model=self.model,
+            temperature=0,
+            base_url=os.getenv("LLM_BASE_URL"),
+            api_key=os.getenv("LLM_API_KEY")
+        )
+        self.agent = await self.create_agent()
+
+    async def create_agent(self):
+    
+        #tools = await load_mcp_tools(session)
+        # Agent prompt
+        prompt = """
+You are a planning agent in a multi agent system.
+Do as you are told.
+"""
+        # Build agent
+        agent = create_agent(
+            model=self.llm,
+            #tools=tools,
+            system_prompt=prompt
+        )
+        return agent
+
+    async def start_agent(self, instruction: str) -> str:
+        """
+        Run the planning agent and extract the full response including plan, context, contact_person, etc.
+        
+        Args:
+            instruction: The instruction/path to process
+            
+        Returns:
+            Dictionary containing contact_person, context, country, documents, and plan
+        """       
+        print("Planning agent asked: ", instruction)
+        # Invoke agent with callbacks
+        result = await self.agent.ainvoke({"messages": [HumanMessage(content=instruction)]}, config={"callbacks": [self.langfuse_handler]})
+        # Extract the last AIMessage content
+        #langfuse = get_client()
+        #langfuse.flush()
+        messages = result.get("messages", [])
+        if not messages:
+            return "No messages received"
+
+        # Get the last message - handle both dict and object formats
+        last_message = messages[-1]
+        return last_message
